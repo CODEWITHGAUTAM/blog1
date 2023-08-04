ff80d0333cd7b23030068bda1dd2afd0b72567ed
CODEWITHGAUTAM <gautamsingh7424@gmail.com>
2023-08-04 16:05:22 +0530

    for loop in angular

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -6,8 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-auth.component.css']
 })
 export class UserAuthComponent {
-  title='switch case';
-  color="red";
+  title='For Loop';
+  users=['Anil','Gautam','Arti','Raman','Pawan','Ankita'];
+
+  userDetails=[
+    {name:'Anil',email:'anil@gmail.com',phone:'8854'},
+    {name:'Gautam',email:'gautam@gmail.com',phone:'7854'},
+    {name:'Arti',email:'arti@gmail.com',phone:'6654'},
+    {name:'Raman',email:'raman@gmail.com',phone:'78754'}
+
+  ];
+  
+
+
+  
   
 
 }
