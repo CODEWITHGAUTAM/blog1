b4aae831450a7f77a2300bcbd669d2ee66a25e94
CODEWITHGAUTAM <gautamsingh7424@gmail.com>
2023-08-04 15:44:13 +0530

    switch case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-      title='blog';
-      getName(name:any)
-      {
-        alert("function called "+name);
-      }
+      
 }
